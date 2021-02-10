8288d17c21151ce228dad03899ed7fdb2c605dd7
thiago.rodrigues <thiago.luiz.rodriguez@gmail.com>
2021-02-09 21:57:42 -0300

    Remove dependencies

--- src/main/java/br/com/tanos/management/record/ports/amqp/ParticipantMessageReceiveImp.java
+++ src/main/java/br/com/tanos/management/record/ports/amqp/ParticipantMessageReceiveImp.java
@@ -3,8 +3,6 @@ package br.com.tanos.management.record.ports.amqp;
 import br.com.tanos.management.record.adapters.amqp.ParticipantMessageReceive;
 import br.com.tanos.management.record.adapters.dto.ParticipantDto;
 import lombok.extern.java.Log;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
